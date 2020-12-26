ec292cefca5a8916227e6f4c8037ac134b9a5ac4
abdelrhman-adel-ahmed <abdelrhman.bedo007@gmail.com>
2020-12-26 22:44:13 +0200

    Update views.py

--- users/views.py
+++ users/views.py
@@ -12,6 +12,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            #if we want to pass the user name to the success message
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
